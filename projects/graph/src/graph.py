ba1ca405d1eaf139e2eff9ff3f68e85978bdd1ea
darenlarson <larson.daren@gmail.com>
2019-04-08 15:42:00 -0500

    Part 1 complete

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,28 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+        
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+        
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist")
+
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+# Continuing from previous example
+graph.add_edge('0', '4')  # No '4' vertex, should raise an Exception!
